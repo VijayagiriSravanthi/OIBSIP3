dd75d1a558e47a4b9eb1ee816f127c33f74a12ce
VijayagiriSravanthi <vijayagirisravanthi0724@gmail.com>
2023-05-17 12:29:43 +0530

    DONE

---
+++ tribute page/script.js
@@ -0,0 +1,11 @@
+window.addEventListener('DOMContentLoaded', function () {
+    var nameElement = document.getElementById('name');
+    var descriptionElement = document.getElementById('description');
+
+    // Replace these values with the person's actual name and description
+    var personName = 'AP J Abdul kalam';
+    var personDescription = 'BIOGRAPHY.';
+
+    nameElement.textContent = personName;
+    descriptionElement.textContent = personDescription;
+});
